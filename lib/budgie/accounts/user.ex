c4b198eba35a981f2dc5121e8e912d62a01f923c
Christian Alexander <christian@linux.com>
2025-02-08 12:04:43 -0700

    Add name to user schema

--- lib/budgie/accounts/user.ex
+++ lib/budgie/accounts/user.ex
@@ -5,6 +5,7 @@ defmodule Budgie.Accounts.User do
   @foreign_key_type :binary_id
   schema "users" do
     field :email, :string
+    field :name, :string
     field :password, :string, virtual: true, redact: true
     field :hashed_password, :string, redact: true
     field :current_password, :string, virtual: true, redact: true
@@ -38,7 +39,7 @@ defmodule Budgie.Accounts.User do
   """
   def registration_changeset(user, attrs, opts \\ []) do
     user
-    |> cast(attrs, [:email, :password])
+    |> cast(attrs, [:email, :password, :name])
     |> validate_email(opts)
     |> validate_password(opts)
   end
