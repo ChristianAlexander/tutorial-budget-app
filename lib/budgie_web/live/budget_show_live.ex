9c3d157c1520d5876878ada7ddc8679fbaa5dc76
Christian Alexander <christian@linux.com>
2025-03-01 16:15:35 -0700

    add currency and transaction amount components

--- lib/budgie_web/live/budget_show_live.ex
+++ lib/budgie_web/live/budget_show_live.ex
@@ -2,6 +2,7 @@ defmodule BudgieWeb.BudgetShowLive do
   use BudgieWeb, :live_view
 
   alias Budgie.Tracking
+  alias Budgie.Tracking.BudgetTransaction
 
   def mount(%{"budget_id" => id}, _session, socket) when is_uuid(id) do
     budget =
@@ -67,8 +68,55 @@ defmodule BudgieWeb.BudgetShowLive do
     <.table id="transactions" rows={@transactions}>
       <:col :let={transaction} label="Description">{transaction.description}</:col>
       <:col :let={transaction} label="Date">{transaction.effective_date}</:col>
-      <:col :let={transaction} label="Amount">{transaction.amount}</:col>
+      <:col :let={transaction} label="Amount"><.transaction_amount transaction={transaction} /></:col>
     </.table>
+    """
+  end
+
+  @doc """
+  Renders a transaction amount as a currency value, considering the type of the transaction.
+
+  ## Example
+
+  <.transaction_amount transaction={%BudgetTransaction{type: :spending, amount: Decimal.new("24.05")}} />
+
+  Output:
+  <span class="tabular-nums text-red-500">-24.05</span>
+  """
+
+  attr :transaction, BudgetTransaction, required: true
+
+  def transaction_amount(%{transaction: %{type: :spending, amount: amount}}),
+    do: currency(%{amount: Decimal.negate(amount)})
+
+  def transaction_amount(%{transaction: %{type: :funding, amount: amount}}),
+    do: currency(%{amount: amount})
+
+  @doc """
+  Renders a currency amount field.
+
+  ## Example
+
+  <.currency amount={Decimal.new("246.01")} />
+
+  Output:
+  <span class="tabular-nums text-green-500">246.01</span>
+  """
+  attr :amount, Decimal, required: true
+  attr :class, :string, default: nil
+  attr :positive_class, :string, default: "text-green-500"
+  attr :negative_class, :string, default: "text-red-500"
+
+  def currency(assigns) do
+    ~H"""
+    <span class={[
+      "tabular-nums",
+      Decimal.gte?(@amount, 0) && @positive_class,
+      Decimal.lt?(@amount, 0) && @negative_class,
+      @class
+    ]}>
+      {Decimal.round(@amount, 2)}
+    </span>
     """
   end
 end
